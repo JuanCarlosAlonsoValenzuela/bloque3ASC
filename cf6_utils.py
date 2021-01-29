be41063a204b83b712700dd8983b3864112259e6
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2021-01-29 18:04:41 +0100

    Refactorization and cf6_16d dataset added

--- cf6_utils.py
+++ cf6_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import math
 
 
 # get pareto front
@@ -8,3 +9,55 @@ def get_pf(dat):
     return df[0].to_numpy(), df[1].to_numpy()
 
 
+# Sign function
+def sgn(sgn_input):
+    if sgn_input > 0.0:
+        return 1.0
+    else:
+        return -1.0
+
+# Compute cf6 and check limits
+def cf6(x):
+    n = x.shape[0]
+
+    # Limits
+    # i = 0 -->     min= 0.0,   max=1.0
+    # i > 0 -->     min= -2.0,  max=2.0
+    # Check limit of x[0]
+    if x[0] < 0.0:
+        x[0] = 0.0
+    if x[0] > 1.0:
+        x[0] = 1.0
+
+    # Check limit of the rest of elements
+    for i in range(1,n):
+        if x[i] > 2.0:
+            x[i] = 2.0
+        if x[i] < -2.0:
+            x[i] = -2.0
+
+    # Initialize out
+    out = np.zeros([2])
+    constr = np.zeros([2])
+
+    # Initialize sum
+    sum1 = np.zeros([1])
+    sum2 = np.zeros([1])
+
+    for j in range(2, n + 1):
+        if j % 2 == 1:
+            yj = x[j-1] - 0.8*x[0]*math.cos(6.0*math.pi*x[0] + (j*math.pi)/n)
+            sum1 = sum1 + math.pow(yj, 2)
+        else:
+            yj = x[j-1] - 0.8*x[0]*math.sin(6.0*math.pi*x[0] + (j*math.pi)/n)
+            sum2 = sum2 + math.pow(yj, 2)
+
+    # Compute out
+    out[0] = x[0] + sum1
+    out[1] = pow(1.0 - x[0], 2) + sum2
+
+    # Compute restrictions
+    constr[0] = x[1] - 0.8*x[0]*math.sin(6.0*x[0]*math.pi+2.0*math.pi/n) - sgn((x[0]-0.5)*(1.0-x[0]))*math.sqrt(abs((x[0]-0.5)*(1.0-x[0])))
+    constr[1] = x[3] - 0.8*x[0]*math.sin(6.0*x[0]*math.pi+4.0*math.pi/n) - sgn(0.25*math.sqrt(1.0-x[0]) - 0.5*(1.0-x[0]))*math.sqrt(abs(0.25*math.sqrt(1.0-x[0])-0.5*(1.0-x[0])))
+
+    return out, constr
--- main_cf6.py
+++ main_cf6.py
@@ -1,14 +1,10 @@
 import numpy as np
 import cf6_utils
 import math
+import cf6_utils
 
 dat = 'PF/PF_cf6.dat'
 
-def sgn(sgn_input):
-    if sgn_input > 0.0:
-        return 1.0
-    else:
-        return -1.0
 
 # Get Pareto Front
 pf_x, pf_y = cf6_utils.get_pf(dat)
@@ -19,41 +15,14 @@ pf_x, pf_y = cf6_utils.get_pf(dat)
 
 # Recibe una x y devuelve el resultado y las restricciones
 
-x = np.array([1.220245e-08, 1.434681e-05, 2.153731e-04, -5.203644e-02])
-# x = np.array([5.758273e-01,	1.161506e-01,	3.459767e-01,	4.545745e-01])
+x = np.array([3.307646e-04,	8.047906e-03,	2.621420e-02,	8.173679e-02,	-1.962023e-03,	2.089632e-02,	3.748047e-02,	1.954840e-02,	6.601718e-03,	2.614146e-02,	-8.654628e-02,	1.739586e-01,	3.176427e-02,	6.292900e-02,	-2.243408e-02,	-7.862677e-02])
 
 
 
 # TODO: Declarar valores mínimos y máximos
-# i = 0 -->     min= 0.0,   max=1.0
-# i > 0 -->     min= -2.0,  max=2.0
 n = x.shape[0]
 
-# Initialize out
-out = np.zeros([2])
-constr = np.zeros([2])
+out, constr = cf6_utils.cf6(x)
 
-# Initialize sum
-sum1 = np.zeros([1])
-sum2 = np.zeros([1])
-
-for j in range(2, n + 1):
-    if j % 2 == 1:
-        yj = x[j-1] - 0.8*x[0]*math.cos(6.0*math.pi*x[0] + (j*math.pi)/n)
-        sum1 = sum1 + math.pow(yj, 2)
-    else:
-        yj = x[j-1] - 0.8*x[0]*math.sin(6.0*math.pi*x[0] + (j*math.pi)/n)
-        sum2 = sum2 + math.pow(yj, 2)
-
-# Compute out
-out[0] = x[0] + sum1
-out[1] = pow(1.0 - x[0], 2) + sum2
-
-# Compute restrictions
-constr[0] = x[1] - 0.8*x[0]*math.sin(6.0*x[0]*math.pi+2.0*math.pi/n) - sgn((x[0]-0.5)*(1.0-x[0]))*math.sqrt(abs((x[0]-0.5)*(1.0-x[0])))
-constr[1] = x[3] - 0.8*x[0]*math.sin(6.0*x[0]*math.pi+4.0*math.pi/n) - sgn(0.25*math.sqrt(1.0-x[0]) - 0.5*(1.0-x[0]))*math.sqrt(abs(0.25*math.sqrt((1.0-x[0])-0.5*(1.0-x[0]))))
-
-# Compute restrictions
 print(out)
 print(constr)
-
